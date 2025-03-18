88e4374a6270e237e78bd298b236cee13599170d
Vitori1 <77644507+Vitori1@users.noreply.github.com>
2025-03-18 19:03:36 -0300

    Update index.js cor

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ class Tarefa {
 
             if (nomeElement && botao) {
                 nomeElement.style.textDecoration = "line-through"; // Aplica o estilo de riscar
-                nomeElement.style.color = "#B1BACB";
+                nomeElement.style.color = "#8F98A8";
 
                 // Substitui o botão por uma imagem
                 const imagem = document.createElement("img");
